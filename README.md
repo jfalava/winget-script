84e6253e44145a770e6ae92b62d87326e6a9b4ee
Jorge Fernando Álava <contacto@jfalava.eu>
2022-08-11 11:56:10 +0200

    Script embellishment and README changes.

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # Winget Master Script for main Windows Machine
-Ninite is overrated and everyone loves a one click install amirite.\
-This script contains all the apps I might need.
+Ninite is overrated and everyone loves a one click install amirite. This script contains all the apps I might need.
 
 You need [winget](https://github.com/microsoft/winget-cli/) for this script to be recognized by your Powershell terminal.
 
 ## How to run
 
 You can either execute the script via terminal with `.\route\to\script\winget-master-script.ps1` or by right clicking the file and selecting `Execute with Powershell`.\
-This is not an unattended install, there will be multiple admin permission prompts, if the application you are installing requires them.\
-If the installation seems to be waiting for something, you might have a window in your taskbar waiting for attention.
+This is not an unattended install, there will bet multiple admin permission prompts, if the application you are installing requires them. If the installation seems to be waiting for something, you might have a windows in your taskbar waiting for attention.
 
 ## Notes
-If you want to make scripts like this you can try [winstall](https://winstall.app/) or use `winget search app.you.want.to.install` to look for new packages and tailor a custom script.\
+If you want to make scripts like this, you can try [winstall](https://winstall.app/) or use `winget search app.you.want.to.install` to look for the packages you need and tailor a custom script from there.\
 Not everything is available in `winget` though, you may ask the developer of the app you want to publish it in the [Windows Package Manager Community Repository](https://docs.microsoft.com/es-es/windows/package-manager/package/repository).
--- winget-master-script.ps1
+++ winget-master-script.ps1
@@ -18,43 +18,48 @@ winget install --id=RiotGames.LeagueOfLegends.EUW -e  ;
 winget install --id=Blitz.Blitz -e  ;
 winget install --id=goatcorp.XIVLauncher -e  ;
 ### Dev Apps ###
-winget install --id=Microsoft.PowerShell -e  ;
 winget install --id=Keybase.Keybase -e  ;
 winget install --id=Microsoft.VisualStudioCode -e  ;
 winget install --id=Git.Git -e  ;
 winget install --id=GitHub.GitHubDesktop -e  ;
 ### Sys Apps ###
+winget install --id=Microsoft.PowerShell -e  ;
 winget install --id=Debian.Debian -e  ;z
 winget install --id=Canonical.Ubuntu -e  ;
+### Media Apps ###
 # Slow download # winget install --id=VideoLAN.VLC -e  ;
 winget install --id=TIDALMusicAS.TIDAL -e  ;
 winget install --id=CiderCollective.Cider -e  ;
-### Internet and Backup Apps ###
-winget install --id=VladimirYakovlev.ElectronMail -e  ;
+winget install --id=Apple.iTunes -e  ;
+### SNS Apps ###
+winget install --id=WhatsApp.WhatsApp.Beta -e  ;
+winget install --id=Telegram.TelegramDesktop -e  ;
+winget install --id=Discord.Discord -e  ;
+### Backup Apps ###
 winget install --id=Mega.MEGASync -e  ;
-winget install --id=BraveSoftware.BraveBrowser -e  ;
-winget install --id=Mozilla.Firefox -e  ;
 winget install --id=Internxt.Drive -e  ;
 winget install --id=dokan-dev.Dokany -e  ;
+### Internet Apps ###
+winget install --id=VladimirYakovlev.ElectronMail -e  ;
+winget install --id=BraveSoftware.BraveBrowser -e  ;
+winget install --id=Mozilla.Firefox -e  ;
 winget install --id=ProtonTechnologies.ProtonVPN -e  ;
 winget install --id=qBittorrent.qBittorrent -e  ;
 winget install --id=NordVPN.NordVPN -e  ;
-winget install --id=Telegram.TelegramDesktop -e  ;
-winget install --id=Discord.Discord -e  ;
+winget install --id=NordPassTeam.NordPass -e  ;
 ### WorkFromHome Apps ###
 winget install --id=AnyDeskSoftwareGmbH.AnyDesk -e  ;
 winget install --id=Microsoft.Teams -e  ;
+winget install --id=Fortinet.FortiClientVPN -e  ;
 # Slow download # winget install --id=GlavSoft.TightVNC -e  ;
-### Tools Apps ###
+### Tool Apps ###
 winget install --id=ModernFlyouts.ModernFlyouts -e  ;
 winget install --id=M2Team.NanaZip -e  ;
 winget install --id=bozbez.WinCaptureAudio.beta -e  ;
 winget install --id=REALiX.HWiNFO -e  ;
 winget install --id=Microsoft.PowerToys -e  ;
 winget install --id=Nota.Gyazo -e  ;
-### Misc Apps ###
-winget install --id=SteelSeries.GG -e  ;
-winget install --id=NordPassTeam.NordPass -e  ;
-winget install --id=Apple.iTunes -e  ;
-winget install --id=Logitech.GHUB -e  ;
 winget install --id=Adobe.Acrobat.Reader.64-bit -e  ;
+### Peripherals Apps ###
+winget install --id=SteelSeries.GG -e  ;
+winget install --id=Logitech.GHUB -e  ;
