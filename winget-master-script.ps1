d0f048e0fe02056022ab4c85e6b576ec91a1d394
Jorge Fernando Álava Vélez <internet.services@jfalava.eu>
2022-10-19 11:55:26 +0200

    UWP Whatsapp and new install order

--- winget-master-script.ps1
+++ winget-master-script.ps1
@@ -32,11 +32,9 @@ winget install --id=Canonical.Ubuntu -e  ;
 winget install --id=TIDALMusicAS.TIDAL -e  ;
 winget install --id=CiderCollective.Cider -e  ;
 ### SNS Apps ###
-# winget install --id=9NKSQGP7F2NH -e --accept-package-agreements ; ## UWP WhatsApp
-# winget install --id=Discord.Discord -e  ; ## Winget version is bugget ATM
+winget install --id=9NKSQGP7F2NH -e --accept-package-agreements ; ## UWP WhatsApp
 winget install --id=XPDC2RH70K22MN -e --accept-package-agreements ; ## Microsoft Store Discord
 winget install --id=Telegram.TelegramDesktop -e  ;
-winget install --id=WhatsApp.WhatsApp.Beta -e  ;
 winget install --id=OpenWhisperSystems.Signal -e  ;
 ### Backup Apps ###
 winget install --id=Mega.MEGASync -e  ;
@@ -47,7 +45,6 @@ winget install --id=9NC73MJWHSWW -e --accept-package-agreements ; ## SyncFolder
 ### WorkFromHome Apps ###
 winget install --id=AnyDeskSoftwareGmbH.AnyDesk -e  ;
 winget install --id=Microsoft.Teams -e  ;
-winget install --id=Fortinet.FortiClientVPN -e  ;
 # winget install --id=GlavSoft.TightVNC -e  ;  ## Slow download
 ### Tool Apps ###
 winget install --id=M2Team.NanaZip -e  ;
@@ -70,7 +67,9 @@ winget install --id=Logitech.GHUB -e  ;
 winget install --id=VladimirYakovlev.ElectronMail -e  ;
 winget install --id=Brave.Brave -e  ;
 winget install --id=Mozilla.Firefox -e  ;
-winget install --id=ProtonTechnologies.ProtonVPN -e  ;
 winget install --id=qBittorrent.qBittorrent -e  ;
-winget install --id=NordVPN.NordVPN -e  ;
 winget install --id=NordPassTeam.NordPass -e  ;
+### VPN and Networking Apps ###
+winget install --id=NordVPN.NordVPN -e  ;
+winget install --id=ProtonTechnologies.ProtonVPN -e  ;
+winget install --id=Fortinet.FortiClientVPN -e  ;
